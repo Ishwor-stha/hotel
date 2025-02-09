fd9825d833bbc116e5e8770626960d3d69354967
Ishwor-stha <ishwor.stha.bank@gmail.com>
2025-02-09 11:02:57 +0545

    added the routes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser=require("cookie-parser")
 const {connect}=require("./db")
 const adminRoute=require("./routes/adminRoute")
 const userRoute=require("./routes/userRoute")
+const bookingRoute=require("./routes/bookingRoute")
 const errorController = require("./controller/errorController")
 const logout=require("./routes/logoutRoute")
 const session=require("express-session")
@@ -25,7 +26,7 @@ app.use(session({
 app.use("/api/admin/",adminRoute)
 app.use("/api/user/",userRoute)
 app.use("/api/",logout)
-
+app.use("/api/user/booking",bookingRoute)
 
 
 connect()
@@ -43,18 +44,3 @@ const port=process.env.PORT ||4000
 app.listen(port,()=>{
     console.log(kleur.blue().bold(`Server is listening on port ${port}.`))
 })
-
-
-//  How Foreign Keys are Passed Internally
-// a) From users Table (user_id)
-
-//     The user_id is typically stored in the session or JWT token upon login.
-//     The backend retrieves it automatically from the session context when processing the request.
-
-// b) From hotels Table (hotel_id)
-
-//     The hotel_id comes from the user's selection on the frontend (e.g., choosing a hotel from a dropdown or list).
-
-// c) From rooms Table (room_id)
-
-//     The room_id comes from the user's selection on the frontend, often after they choose a hotel. The available rooms are fetched dynamically based on the selected hotel.
---
+++ routes/bookingRoute.js
@@ -0,0 +1,4 @@
+const Router=require("express").Router();
+const {chooseHotel}=require("../controller/bookingController")
+Router.route("/booking")
+module.exports=Router
