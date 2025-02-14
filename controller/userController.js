7a1fb8660e4b2c0b951643c5ef13027c83e71df4
Ishwor-stha <ishwor.stha.bank@gmail.com>
2025-02-14 12:30:40 +0545

    removing unnecessary rouutes form user

--- controller/userController.js
+++ controller/userController.js
@@ -11,18 +11,23 @@ const {
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
+// @desc:Controller to create a new user
+// @method:POST
+// @endPoint:localhost:4000/api/user/create-user
 module.exports.createUser = async (req, res, next) => {
     try {
         if (!req.body) return next(new errorHandling("Empty body: Ensure you're sending the correct information.", 400));
+        // list all possible keys
         const possibleFields = ["firstName", "lastName", "email", "dob", "country", "gender", "city", "zip", "address", "password", "phone2", "phone", "confirmPassword"];
         
+
         for (const key in req.body) {
             if (!possibleFields.includes(key)) {
                 return next(new errorHandling("All fields are required, please fill up the form", 400));
             }
         }
-        
-        // list all possible keys
+
+        // destructing the req.body object
         const {
             firstName,
             middleName,
@@ -41,22 +46,26 @@ module.exports.createUser = async (req, res, next) => {
         } = req.body;
         
         let fullName;
+        // no middle name is given
         if (!middleName) {
             fullName = `${firstName} ${lastName}`;
         } else {
             fullName = `${firstName} ${middleName} ${lastName}`;
         }
 
+        //email validation 
         if (!validateEmail(email)) return next(new errorHandling(400,"Please enter a valid email address"));
-
+        //phone no validation
         if (!isValidNepaliPhoneNumber(phone)) return next(new errorHandling(400,"Please enter a valid phone number."));
 
         if (!isValidNepaliPhoneNumber(phone2)) return next(new errorHandling(400,"Please enter a valid phone number."));
-
+        // check password and confirm password matches
         if (password !== confirmPassword) return next(new errorHandling(400,"Password and confirm password do not match"));
-        
+        // hash password
         const hashedPassword = bcrypt.hashSync(password);
+        // mysql query
         const query = `INSERT INTO users (name, email, dob, gender, address, password, phone, phone2, country, city, zip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        //inserting the user details in database
         const createUser = await connection.promise().query(query, [fullName, email, dob, gender, address, hashedPassword, phone, phone2, country, city, zip]);
         
         res.status(200).json({
@@ -64,14 +73,18 @@ module.exports.createUser = async (req, res, next) => {
             "message": fullName + " created successfully"
         });
     } catch (error) {
+        // email duplication error
         if (error.code === "ER_DUP_ENTRY") return next(new errorHandling(500,"Email address already used, please try another."));
         return next(new errorHandling(500,error.message));
     }
 }
 
-
+// @desc:Controller to login by user
+// @method:POST
+// @endPoint:localhost:4000/api/user/login
 module.exports.login = async (req, res, next) => {
     try {
+        //length of  keys in req.body must be 2 
         if (Object.keys(req.body).length > 2) return next(new errorHandling(400, "Something went wrong.Please try again."))
         // Taking userName from client side
         const userEmail = req.body.email
@@ -80,22 +93,31 @@ module.exports.login = async (req, res, next) => {
         // it there is no user name or password then terminate current middleware and call errorhandling middleware with two argument ie(errormessage,statusCode)
         if (!userEmail || !userPassword) return next(new errorHandling(400, "Email or password field is empty."))
         if (!validateEmail(userEmail)) return next(new errorHandling(400, "Please enter valid email address."))
+        
+        // query for sql 
         const query = `SELECT * FROM users WHERE email = ?`;
+        // fetching data form database
         const [userDetail] = await connection.promise().query(query, [userEmail])
         // if there is no userDetail then terminate current middleware and call errorhandling middleware
         if (userDetail.length === 0) return next(new errorHandling(401, "Incorrect email or password.Please try again."))
+        // password form database
         dbPassword = userDetail[0].password
+        // name from database
         dbName = userDetail[0].name
+        // comparing password
         const validPassword = await bcrypt.compare(userPassword, dbPassword) //true/false
         // if password doesnot match then terminate this/current middleware and call error handling middleware
         if (!validPassword) return next(new errorHandling(401, "Incorrect email or password.Please try again."))
+        // creating payload for jwt token
         const payload = {
             "id": userDetail[0].id,
             "email": userDetail[0].email
         }
-        const token = jwt.sign(payload, process.env.jwt_secret_key, {
+        // generating jwt token
+        const token = await jwt.sign(payload, process.env.jwt_secret_key, {
             expiresIn: process.env.jwt_expiry
         })
+        // sending the token to user
         res.cookie("auth_token", token, {
             httpOnly: true,
             sameSite: "Strict",
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,8 @@
 const Router=require("express").Router()
 const {createUser,login}=require("../controller/userController")
-const {chooseHotel,chooseRoom/*,paymentDetails*/}=require("../controller/bookingController")
 
 
 
-Router.route("/hotel").post(chooseHotel)
-Router.route("/room").post(chooseRoom)
 Router.route("/create-user").post(createUser)
 Router.route("/login").post(login)
 // Router.route("/details").post(paymentDetails)
