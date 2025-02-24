b743151ee6f21aea25c80728a77957bbf7bffa04
Ishwor-stha <ishwor.stha.bank@gmail.com>
2025-02-24 16:17:50 +0545

    minor change

--- routes/logoutRoute.js
+++ routes/logoutRoute.js
@@ -1,7 +1,7 @@
+const Router=require("express").Router()
 const { logout } = require("../controller/logout")
 const {checkJwt}=require("../controller/adminController")
 
-const Router=require("express").Router()
 
 Router.route("/logout").delete(checkJwt, logout)
 
