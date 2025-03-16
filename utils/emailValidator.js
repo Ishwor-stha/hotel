060318f6b1c47ac205096018c84ffa3241cbd9d4
ishwor-stha <ishwor.stha.bank@gmail.com>
2025-03-16 16:15:45 +0545

    added more options

--- utils/emailValidator.js
+++ utils/emailValidator.js
@@ -1,9 +1,9 @@
 const validator = require("validator");
 
 
-module.exports. validateEmail = (email) => {
+module.exports.validateEmail = (email) => {
     const isEmail = validator.isEmail(email);
-    const allowedDomains = ["gmail.com", "yahoo.com", "outlook.com"];
+    const allowedDomains = ["gmail.com", "yahoo.com", "outlook.com","ymail.com","icloud.com","protonmail.com","edu"];
     const emailDomain = email.split('@')[1]; 
-    return isEmail && allowedDomains.includes(emailDomain);
+    return isEmail && allowedDomains.includes(emailDomain)||emailDomain.endsWith(".edu");;
 };
