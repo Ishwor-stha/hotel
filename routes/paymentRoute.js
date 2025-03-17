26a0e0a3d9be8482d5f8fb6c9e9e5127200aeee1
ishwor-stha <ishwor.stha.bank@gmail.com>
2025-03-17 12:07:30 +0545

    added the jwt middleware in payments

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const {checkJwt}=require("../controller/adminController")
 const Router = express.Router();
 const {payWithEsewa,failure,success} = require("../controller/paymentController");
 
 
-Router.route("/make-payment").post(payWithEsewa);
-Router.route("/success").get(success);
-Router.route("/failed").get(failure);
+Router.route("/make-payment").post(checkJwt,payWithEsewa);
+Router.route("/success").get(checkJwt,success);
+Router.route("/failed").get(checkJwt,failure);
 
 
 module.exports = Router;
