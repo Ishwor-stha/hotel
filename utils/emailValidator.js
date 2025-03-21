ddec80bbdeb2995ca28189c59f895743777d0c16
ishwor-stha <ishwor.stha.bank@gmail.com>
2025-03-21 11:45:12 +0545

    fixed the invalid email issue

--- utils/emailValidator.js
+++ utils/emailValidator.js
@@ -4,6 +4,7 @@ const validator = require("validator");
 module.exports.validateEmail = (email) => {
     const isEmail = validator.isEmail(email);
     const allowedDomains = ["gmail.com", "yahoo.com", "outlook.com","ymail.com","icloud.com","protonmail.com","edu"];
-    const emailDomain = email.split('@')[1]; 
-    return isEmail && allowedDomains.includes(emailDomain)||emailDomain.endsWith(".edu");;
+    const emailDomain = email.split('@')[1];
+    if(!emailDomain )return false
+    return isEmail && allowedDomains.includes(emailDomain) || emailDomain.endsWith(".edu");;
 };
